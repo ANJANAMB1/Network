4e3d609397439df045eb42d184ba99963a2bad43
ANJANAMB1 <92799111+ANJANAMB1@users.noreply.github.com>
2023-04-05 11:45:49 +0530

    Add files via upload

---
+++ operation.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+a=10
+b=20
+
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
